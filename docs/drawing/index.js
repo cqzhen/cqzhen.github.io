4c6960f87da01924cd65a73171f16513362cb3d1
cqzhen <1071535051@qq.com>
2018-08-11 22:58:03 +0800

    优化样式

--- docs/drawing/index.js
+++ docs/drawing/index.js
@@ -1,26 +1,27 @@
 {
   let canvas = document.getElementById('cqzhen_drawing');
+  let canvas_widthHalf = canvas.width / 2;
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
     
     ctx.beginPath();
-    ctx.arc(75,75,50,0,Math.PI*2,true); // 绘制
-    ctx.moveTo(100,120);
-    ctx.arc(75,120,25,0,1*Math.PI,true);   // 口(顺时针)
-    ctx.moveTo(65,65);
-    ctx.arc(60,65,5,0,Math.PI*2,true);  // 左眼
-    ctx.moveTo(95,65);
-    ctx.arc(90,65,5,0,Math.PI*2,true);  // 右眼
+    ctx.arc(canvas_widthHalf,75,50,0,Math.PI*2,true); // 绘制
+    ctx.moveTo(canvas_widthHalf + 15, 100);
+    ctx.arc(canvas_widthHalf,100,15,0,1*Math.PI,true);   // 口(顺时针)
+    ctx.moveTo(canvas_widthHalf - 15,65);
+    ctx.arc(canvas_widthHalf - 20,65,5,0,Math.PI*2,true);  // 左眼
+    ctx.moveTo(canvas_widthHalf + 20,65);
+    ctx.arc(canvas_widthHalf + 15,65,5,0,Math.PI*2,true);  // 右眼
     ctx.stroke();
     
     ctx.beginPath();
-    ctx.arc(75,235,50,0,Math.PI*2,true); // 绘制
-    ctx.moveTo(110,235);
-    ctx.arc(75,235,35,0,Math.PI,false);   // 口(顺时针)
-    ctx.moveTo(65,225);
-    ctx.arc(60,225,5,0,Math.PI*2,true);  // 左眼
-    ctx.moveTo(95,225);
-    ctx.arc(90,225,5,0,Math.PI*2,true);  // 右眼
+    ctx.arc(canvas_widthHalf,235,50,0,Math.PI*2,true); // 绘制
+    ctx.moveTo(canvas_widthHalf + 35,235);
+    ctx.arc(canvas_widthHalf,235,35,0,Math.PI,false);   // 口(顺时针)
+    ctx.moveTo(canvas_widthHalf - 15,225);
+    ctx.arc(canvas_widthHalf - 20,225,5,0,Math.PI*2,true);  // 左眼
+    ctx.moveTo(canvas_widthHalf + 20,225);
+    ctx.arc(canvas_widthHalf + 15,225,5,0,Math.PI*2,true);  // 右眼
     ctx.stroke();
     
     // TODO: robot
