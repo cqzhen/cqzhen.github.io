c23fea1a49b0f49d1cbd7f2e4523c577b2222fac
cqzhen <1071535051@qq.com>
2018-08-13 22:47:46 +0800

    drawing args

--- docs/drawing/index.js
+++ docs/drawing/index.js
@@ -4,35 +4,37 @@
   if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
     
-    drawingCry(canvas_widthHalf);
-    drawingSmile(canvas_widthHalf); 
+    drawingCry(canvas_widthHalf, 75);
+    drawingSmile(canvas_widthHalf, 235); 
     // TODO: robot
     drawingRobot(canvas_widthHalf, 340);
 
     // TODO: animation
   }
 
-  function drawingCry(canvas_widthHalf) {
+  function drawingCry(canvas_widthHalf, y) {
+    //  y = 75
     ctx.beginPath();
-    ctx.arc(canvas_widthHalf,75,50,0,Math.PI*2,true); // 绘制
-    ctx.moveTo(canvas_widthHalf + 15, 100);
-    ctx.arc(canvas_widthHalf,100,15,0,1*Math.PI,true);   // 口(顺时针)
-    ctx.moveTo(canvas_widthHalf - 15,65);
-    ctx.arc(canvas_widthHalf - 20,65,5,0,Math.PI*2,true);  // 左眼
-    ctx.moveTo(canvas_widthHalf + 20,65);
-    ctx.arc(canvas_widthHalf + 15,65,5,0,Math.PI*2,true);  // 右眼
+    ctx.arc(canvas_widthHalf,y,50,0,Math.PI*2,true); // 绘制
+    ctx.moveTo(canvas_widthHalf + 15, y + 25);
+    ctx.arc(canvas_widthHalf, y + 25 , 15, 0, 1 * Math.PI, true);   // 口(顺时针)
+    ctx.moveTo(canvas_widthHalf - 15, y - 10);
+    ctx.arc(canvas_widthHalf - 20, y - 10 , 5, 0, Math.PI * 2, true);  // 左眼
+    ctx.moveTo(canvas_widthHalf + 20, y - 10);
+    ctx.arc(canvas_widthHalf + 15, y - 10, 5, 0, Math.PI * 2, true);  // 右眼
     ctx.stroke();
   }
 
-  function drawingSmile(canvas_widthHalf) {
+  function drawingSmile(canvas_widthHalf, y) {
+    //  y = 235
     ctx.beginPath();
-    ctx.arc(canvas_widthHalf,235,50,0,Math.PI*2,true); // 绘制
-    ctx.moveTo(canvas_widthHalf + 35,235);
-    ctx.arc(canvas_widthHalf,235,35,0,Math.PI,false);   // 口(顺时针)
-    ctx.moveTo(canvas_widthHalf - 15,225);
-    ctx.arc(canvas_widthHalf - 20,225,5,0,Math.PI*2,true);  // 左眼
-    ctx.moveTo(canvas_widthHalf + 20,225);
-    ctx.arc(canvas_widthHalf + 15,225,5,0,Math.PI*2,true);  // 右眼
+    ctx.arc(canvas_widthHalf, y, 50, 0, Math.PI * 2, true);  // 绘制
+    ctx.moveTo(canvas_widthHalf + 35, y);
+    ctx.arc(canvas_widthHalf, y, 35, 0, Math.PI, false);  // 口(顺时针)
+    ctx.moveTo(canvas_widthHalf - 15, y - 10);
+    ctx.arc(canvas_widthHalf - 20, y - 10, 5, 0, Math.PI * 2, true);  // 左眼
+    ctx.moveTo(canvas_widthHalf + 20, y - 10);
+    ctx.arc(canvas_widthHalf + 15, y - 10, 5, 0, Math.PI * 2, true);  // 右眼
     ctx.stroke();
   }
 
