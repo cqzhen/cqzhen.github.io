c2f1370c2e2a4baccf5c7f08331f59086f2ece5d
cqzhen <1071535051@qq.com>
2019-11-09 12:34:11 +0800

    add snow

--- js/blog.js
+++ js/blog.js
@@ -1,34 +1,58 @@
 (function(){
 	const blogData = [
 		{
-			title: 'Js 基础篇',
-			date: '2019-11-07',
+			title: '闲谈 Http',
+			date: '2019-11-09',
 			type: 3,
-			img: 'http://img3.imgtn.bdimg.com/it/u=620565823,2221239087&fm=214&gp=0.jpg',
-			text: '主要介绍 Js 基础内容',
-			href: 'article/js/Js基础篇.md'
+			img: 'https://s2.ax1x.com/2019/11/09/Me2v26.jpg',
+			text: '主要介绍 Http 基础内容',
+			href: 'article/js/闲谈Http'
+		},
+		{
+			title: '江雪',
+			date: '2019-11-09',
+			type: 1,
+			img: 'https://s2.ax1x.com/2019/11/09/MecDit.jpg',
+			text: '千山鸟飞绝，万径人踪灭。孤舟蓑笠翁，独钓寒江雪。',
+			href: 'article/life/jiangxue'
 		},
 		{
 			title: '闲谈 Cookie',
-			date: '2019-11-07',
+			date: '2019-11-08',
 			type: 3,
-			img: 'http://img3.imgtn.bdimg.com/it/u=620565823,2221239087&fm=214&gp=0.jpg',
+			img: 'https://s2.ax1x.com/2019/11/09/Me2v26.jpg',
 			text: '主要介绍 Cookie 基础内容',
 			href: 'article/js/闲谈Cookie'
 		},
 		{
-			title: '闲谈 Http',
+			title: '进阶 Vim',
+			date: '2019-11-07',
+			type: 7,
+			img: 'https://s2.ax1x.com/2019/11/09/Megtf0.jpg',
+			text: '进阶 Vim',
+			href: 'article/tool/vim/'
+		},
+		{
+			title: '谈谈如何解脱劳动力',
+			date: '2019-11-07',
+			type: 1,
+			img: 'https://s2.ax1x.com/2019/11/09/MegI7d.jpg',
+			text: '猜猜是什么内容，也许将来你的工作就是这个走向',
+			href: 'article/life/draw/index.html'
+		},
+		{
+			title: 'Js 基础篇',
 			date: '2019-11-07',
 			type: 3,
-			img: 'http://img3.imgtn.bdimg.com/it/u=620565823,2221239087&fm=214&gp=0.jpg',
-			text: '主要介绍 Http 基础内容',
-			href: 'article/js/闲谈Http.md'
+			img: 'https://s2.ax1x.com/2019/11/09/Me2v26.jpg',
+			text: '主要介绍 Js 基础内容',
+			href: 'article/js/Js基础篇.md'
 		},
 		{
 			title: '闲谈 Promise',
 			date: '2019-11-07',
 			type: 3,
-			img: 'http://img3.imgtn.bdimg.com/it/u=620565823,2221239087&fm=214&gp=0.jpg',
+			img: 'https://s2.ax1x.com/2019/11/09/Me2v26.jpg',
 			text: '主要介绍 Promise 基础内容',
 			href: 'article/js/闲谈Promise.md'
 		},
@@ -36,7 +60,7 @@
 			title: '闲谈 Async',
 			date: '2019-11-07',
 			type: 3,
-			img: 'http://img3.imgtn.bdimg.com/it/u=620565823,2221239087&fm=214&gp=0.jpg',
+			img: 'https://s2.ax1x.com/2019/11/09/Me2v26.jpg',
 			text: '主要介绍 Async 基础内容',
 			href: 'article/js/闲谈Async.md'
 		},
@@ -44,25 +68,9 @@
 			title: '闲谈 Generator',
 			date: '2019-11-07',
 			type: 3,
-			img: 'http://img3.imgtn.bdimg.com/it/u=620565823,2221239087&fm=214&gp=0.jpg',
+			img: 'https://s2.ax1x.com/2019/11/09/Me2v26.jpg',
 			text: '主要介绍 Generator 基础内容',
 			href: 'article/js/闲谈Generator.md'
-		},
-		{
-			title: '谈谈如何解脱劳动力',
-			date: '2019-11-07',
-			type: 1,
-			img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1573114383692&di=c90c95f4556cc6d2bc31b60ce4722779&imgtype=0&src=http://img2.ctoutiao.com/uploads/2018/11/16/1542351063396881.jpg',
-			text: '猜猜是什么内容，也许将来你的工作就是这个走向',
-			href: 'article/life/draw/index.html'
-		},
-		{
-			title: '进阶 Vim',
-			date: '2019-11-07',
-			type: 7,
-			img: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1573116308395&di=d15e873b4408869b940a5ef0b8bae8ae&imgtype=0&src=http://blog.chinaunix.net/attachment/201604/7/31111745_1459962266z0lF.png',
-			text: '进阶 Vim',
-			href: 'article/tool/vim/'
 		}
 	];
 
