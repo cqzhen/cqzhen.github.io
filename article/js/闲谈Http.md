b3cfc2854c52dcc90e52367261ba02d6aa4171c5
cqzhen <1071535051@qq.com>
2019-11-11 11:37:10 +0800

    switch type

---
+++ article/js/闲谈Http.md
@@ -0,0 +1,16 @@
+[返回导航页](https://cqzhen.github.io/blog.html "导航页面")
+
+# HTTP
+
+### 为什么会有 Http ?
+
+
+### 为什么用 Http 不用其它的，比如？
+
+
+### Http 详解
+
+
+### Https 和 Http 有什么区别？
+
+HTTPS (HTTP Secure) is an encrypted version of the HTTP protocol. It usually uses SSL or TLS to encrypt all communication between a client and a server. This secure connection allows clients to safely exchange sensitive data with a server, for example for banking activities or online shopping.
--- js/blog.js
+++ js/blog.js
@@ -75,6 +75,7 @@
 	];
 
 	const blogType = {
+		All: 0,
 		Life: 1,
 		Book: 2,
 		JS: 3,
@@ -88,34 +89,66 @@
 		return types;
 	}
 
-	function renderType() {
+	function renderType(cur = 'All') {
 		let types = getType();
 		let text = '',
 				typeContainer = document.getElementsByClassName('blogType')[0];
 		for (let i = 0; i < types.length; i++) {
-			text += '<label type=' + types[i] + '>' + types[i] + '</label>';
+			if (types[i] == cur) {
+				text += '<label style="background:#007a80" type=' + types[i] + '>' + types[i] + '</label>';
+			} else {
+				text += '<label type=' + types[i] + '>' + types[i] + '</label>';
+			}
 		}
 		typeContainer.innerHTML = text;
 	}
 
-	function getBlog() {
-		return blogData;
+	function updateType(target) {
+		document.querySelectorAll('.blogType label').forEach(item => item.style.background = '#daebec');
+		target.style.background = '#007a80';
 	}
 
-	function renderBlog() {
-		let blogs = getBlog();
+	function getBlog(type = 'All') {
+		type = blogType[type];
+
+		if (!type) return blogData;
+
+		return blogData.filter(item => item.type === type);
+	}
+
+	function renderBlog(type) {
+		let blogs = getBlog(type);
 		let text = '',
 				blogContainer = document.getElementsByClassName('blogContext')[0];
 		for (let i = 0; i < blogs.length; i++) {
 			text += '<div><a href="' + blogs[i].href + '"><h3>' + blogs[i].title + '</h3><span>' + blogs[i].date + '</span><div><img src="' +blogs[i].img + '"></img><span>' + blogs[i].text + '</span></div></a></div>';
 		}
 
+		if (!blogs.length) text = '<p style="text-align:center">抱歉，暂无 ' + type + ' 相关内容。</p>';
+
 		blogContainer.innerHTML = text;
 	}
 
+	function addEvent() {
+		let typeContainer = document.getElementsByClassName('blogType')[0];
+		typeContainer.onclick = function(event) {
+			let target = event.target;
+			switch(target.tagName) {
+				case 'LABEL':
+					renderBlog(target.attributes.type.value);
+					updateType(target);
+					break;
+				default:
+					console.log('haha');
+					break;
+			}
+		}
+	}
+
 	function init() {
-		renderType();
-		renderBlog();
+		renderType('All');
+		renderBlog('All');
+		addEvent();
 	}
 
 	init();
